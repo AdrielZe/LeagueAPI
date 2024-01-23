3942ea0e2f6151296cee1b27c42c3b6327729ed1
adrielze <oadriel14@gmail.com>
2024-01-23 18:03:42 -0300

    Função que encontra o PUUID do invocador através do nome

--- src/main/java/RiotAPI/Riot/services/FindPuuidService.java
+++ src/main/java/RiotAPI/Riot/services/FindPuuidService.java
@@ -8,7 +8,7 @@ public class FindPuuidService {
     public String getPuuidFromApi(@PathVariable("summoner") String summoner ){
         RestTemplate restTemplate = new RestTemplate();
 
-        QuerySummonerDTO querySummonerDTO = restTemplate.getForObject(String.format("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s?api_key=RGAPI-e94bd0e4-3f2f-48e5-8fe9-4c143bf8e50c", summoner), QuerySummonerDTO.class);
+        QuerySummonerDTO querySummonerDTO = restTemplate.getForObject(String.format("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s?api_key=RGAPI-dfdfe0d2-8032-4891-a7ec-9bd2a75ceb69", summoner), QuerySummonerDTO.class);
 
         return querySummonerDTO.getPuuid();
     }
