27b090365454e7175cffe504d3e3fd4ef26eb8a3
adrielze <oadriel14@gmail.com>
2024-01-23 18:02:26 -0300

    Adicionando função que calcula a última data em que o jogador jogou com cada campeão

--- src/main/java/RiotAPI/Riot/services/CalculateLastDatePlayedService.java
+++ src/main/java/RiotAPI/Riot/services/CalculateLastDatePlayedService.java
@@ -1,2 +1,15 @@
-package RiotAPI.Riot.services;public class CalculateLastDatePlayedService {
+package RiotAPI.Riot.services;
+
+import java.time.Instant;
+import java.time.InstantSource;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class CalculateLastDatePlayedService {
+
+    public LocalDateTime calculateLastDatePlayed(long unix){
+        Instant instant = Instant.ofEpochMilli(unix);
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return localDateTime;
+    }
 }
