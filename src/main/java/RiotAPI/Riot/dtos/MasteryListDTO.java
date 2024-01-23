feadc8e415e658dea4c42f9674e85eb30a1c3f36
adrielze <oadriel14@gmail.com>
2024-01-23 18:01:12 -0300

    Adicionando objeto para manipular o array de maestria de campeões

--- src/main/java/RiotAPI/Riot/dtos/MasteryListDTO.java
+++ src/main/java/RiotAPI/Riot/dtos/MasteryListDTO.java
@@ -1,2 +1,53 @@
-package RiotAPI.Riot.dtos;public class MasteryListDTO {
+package RiotAPI.Riot.dtos;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@RestController
+public class MasteryListDTO {
+    private long championId;
+
+    private int masteryLevel;
+
+    private String championName;
+
+    public LocalDateTime getLastDatePlayed() {
+        return lastDatePlayed;
+    }
+
+    public void setLastDatePlayed(LocalDateTime lastDatePlayed) {
+        this.lastDatePlayed = lastDatePlayed;
+    }
+
+    private LocalDateTime lastDatePlayed;
+
+    public long getChampionId() {
+        return championId;
+    }
+
+    public void setChampionId(long championId) {
+        this.championId = championId;
+    }
+
+    public int getMasteryLevel() {
+        return masteryLevel;
+    }
+
+    public void setMasteryLevel(int masteryLevel) {
+        this.masteryLevel = masteryLevel;
+    }
+
+    public String getChampionName() {
+        return championName;
+    }
+
+    public void setChampionName(String championName) {
+        this.championName = championName;
+    }
+
+
 }
